af6d8dba43bbf76b51e5142ea4dba9b69a002d3c
lbruhin <22396688+lbruhin@users.noreply.github.com>
2020-12-01 16:32:51 +0100

    fixed tab issue

--- day-01/part-1/lena.py
+++ day-01/part-1/lena.py
@@ -5,9 +5,9 @@ class LenaSubmission(SubmissionPy):
 
     def run(self, s):
         List = s.split()
-		map_list = map(int, List)
-		list_of_integers = list(map_list)
-		for n, num in enumerate(list_of_integers):
-			wanted = 2020 - num
-			if wanted in list_of_integers[n:]:
-				return (num * wanted)
+        map_list = map(int, List)
+        list_of_integers = list(map_list)
+        for n, num in enumerate(list_of_integers):
+            wanted = 2020 - num
+            if wanted in list_of_integers[n:]:
+                return (num * wanted)
--- day-01/part-2/lena.py
+++ day-01/part-2/lena.py
@@ -5,12 +5,12 @@ class LenaSubmission(SubmissionPy):
 
     def run(self, s):
         List = s.split()
-		map_list = map(int, List)
-		list_of_integers = list(map_list)
-		numbers = sorted(list_of_integers)
-		l = len(numbers)
-		for i in range(0,l-2):
-			for j in range(i+1, l-1):
-				for k in range(j+1, l):
-					if numbers[i]+numbers[j]+numbers[k] == 2020:
-						return numbers[i]*numbers[j]*numbers[k]
+        map_list = map(int, List)
+        list_of_integers = list(map_list)
+        numbers = sorted(list_of_integers)
+        l = len(numbers)
+        for i in range(0, l - 2):
+            for j in range(i + 1, l - 1):
+                for k in range(j + 1, l):
+                    if numbers[i] + numbers[j] + numbers[k] == 2020:
+                        return numbers[i] * numbers[j] * numbers[k]
