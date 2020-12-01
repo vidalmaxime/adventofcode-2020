eb702a287aef5b6122274cedab4dd61a48739428
Maxime Vidal <mvidal@student.ethz.ch>
2020-12-01 16:50:01 +0100

    Switch from 3.7 to 3.9

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -ev
 
 # source $HOME/.cargo/env
 export PATH=$PATH:~/.cargo/bin:$GOROOT/bin
-export PYENV_VERSION=3.7
+export PYENV_VERSION=3.9
 
 git --no-pager diff --name-only origin/master -- | grep "day-" | cut -d "/" -f1 | cut -d "-" -f2 | sort | uniq | xargs -I{} ./aoc run -fd {}
