0110809687007bd62daf17e6940345bfcf9d7623
lbruhin <22396688+lbruhin@users.noreply.github.com>
2020-12-05 12:27:49 +0100

    tab error

--- day-05/part-2/lena.py
+++ day-05/part-2/lena.py
@@ -10,7 +10,7 @@ class LenaSubmission(SubmissionPy):
         for seat in seats:
         	binary = seat.replace('F','0').replace('B', '1').replace('L','0').replace('R', '1')
         	numbers.append(int(binary[:-3],2)*8 + int(binary[-3:],2))
-        	missing = set(range(min(numbers), max(numbers)))-set(numbers)
+            missing = set(range(min(numbers), max(numbers)))-set(numbers)
 
         return list(missing)[0]
 
