6fb161a04162cdfba9c1b732883e31506a9e4adb
vbruttin <valentin.bruttin@epfl.ch>
2020-12-07 10:28:46 +0100

    remove numpy dependency and optimize code

--- day-05/part-1/vbruttin.py
+++ day-05/part-1/vbruttin.py
@@ -3,9 +3,6 @@ from tool.runners.python import SubmissionPy
 
 class VbruttinSubmission(SubmissionPy):
     def run(self, s):
-        max_id = 0
-        for seat in s.split('\n'):
-            row = int(seat[:7].replace('F', '0').replace('B', '1'), 2)
-            column = int(seat[7:].replace('L', '0').replace('R', '1'), 2)
-            max_id = max(max_id, row * 8 + column)
-        return max_id
+        return max(
+            int(seat.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
+            for seat in s.split('\n'))
--- day-05/part-2/vbruttin.py
+++ day-05/part-2/vbruttin.py
@@ -4,11 +4,11 @@ import numpy as np
 
 class VbruttinSubmission(SubmissionPy):
     def run(self, s):
-        occupied_seats = []
-        for seat in s.split('\n'):
-            row = int(seat[:7].replace('F', '0').replace('B', '1'), 2)
-            column = int(seat[7:].replace('L', '0').replace('R', '1'), 2)
-            occupied_seats.append(row * 8 + column)
-        occupied_seats = np.sort(np.array(occupied_seats))
-        missing_index = np.argmax(np.diff(occupied_seats))
-        return occupied_seats[missing_index] + 1
+        occupied_seats = [
+            int(seat.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
+            for seat in s.split('\n')
+        ]
+        occupied_seats.sort()
+        for s1, s2 in zip(occupied_seats[:-1], occupied_seats[1:]):
+            if s2 - s1 == 2:
+                return s1 + 1
