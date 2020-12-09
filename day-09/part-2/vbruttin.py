3c0d646e83b96d01074e08160dfd6b20b90697ae
vbruttin <valentin.bruttin@epfl.ch>
2020-12-09 11:34:34 +0100

    solve part 1 in part 2 script to avoid errors on other inputs

--- day-09/part-2/vbruttin.py
+++ day-09/part-2/vbruttin.py
@@ -1,11 +1,19 @@
 from tool.runners.python import SubmissionPy
+from itertools import combinations
 
 
 class VbruttinSubmission(SubmissionPy):
     def run(self, s):
         sequence = list(map(int, s.split('\n')))
         length = len(sequence)
-        target = 31161678
+        for i in range(length - 26):
+            target = sequence[i + 25]
+            for s in combinations(sequence[i:i + 25], 2):
+                if sum(s) == target:
+                    break
+            else:
+                break
+        # target = 31161678
         for i in range(length):
             stack = sequence[i]
             for j in range(i + 1, length):
