6b3ac0be6d0941335b09ec33a240a2b781b235f0
lbruhin <22396688+lbruhin@users.noreply.github.com>
2020-12-10 11:00:30 +0100

    no import solution

--- day-10/part-1/lena.py
+++ day-10/part-1/lena.py
@@ -1,10 +1,16 @@
 from tool.runners.python import SubmissionPy
-from numpy import array, diff
 
 class LenaSubmission(SubmissionPy):
 
     def run(self, s):
         s = list(sorted(map(int,s.split('\n'))))
         s = [0]+s+[max(s)+3]
-        d = array(diff(s))
-        return(sum(d ==1)*sum(d==3))
+        #d = list([s[i+1]-s[i] for i in range(len(s)-1)])
+        ones = 0
+        threes = 0
+        for low, high in zip(s, s[1:]):
+            if high - low == 3:
+                threes += 1
+            elif high - low == 1:
+                ones += 1
+        return ones*threes
