3becbf88d3009975a161c8553335dc16c4b4e5d4
LucZot <lucas.stoffl@tum.de>
2020-12-15 22:56:33 +0100

    luc day 15

---
+++ day-15/part-1/lucas.py
@@ -0,0 +1,22 @@
+from tool.runners.python import SubmissionPy
+
+
+class LucasSubmission(SubmissionPy):
+
+    def run(self, s):
+        # :param s: input in string format
+        data = [int(x) for x in s.split(',')]
+        numbers = dict()
+        numbers = {**numbers, **{data[i]: [i + 1] for i in range(len(data))}}
+        last = data[-1]
+        for i in range(len(data) + 1, 2020 + 1):
+            if len(numbers[last]) > 1:
+                last = numbers[last][-1] - numbers[last][-2]
+            else:
+                last = 0
+            if last in numbers.keys():
+                numbers[last].append(i)
+            else:
+                numbers[last] = [i]
+        return last
+        
